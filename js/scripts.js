ef34940fa41626b782bb66ab8fbd39b50160afe6
Ben Reckas <benreckas@gmail.com>
2017-11-01 23:15:29 -0700

    Day 2 take home completed. Added comments.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,17 @@
+// Asks for users first name and logs answer to console
 var firstName = prompt("What is your first name?");
 console.log(firstName);
+// Asks for users last name and alerts the answer in the window
 var lastName = prompt("What is your last name?");
 var alertName = alert(`Whassup, ${lastName}!`);
-
+// Asks for users birthday
 var birthday = prompt("Gimme your birthday.\nMM/DD/YYYY");
+// Inserts users birthday into Date object
 var birthdayDate = new Date(birthday);
-
+// Retrieves the month day and year and assigns them in a string to confirmDate
 var month = birthdayDate.getUTCMonth() + 1;
 var day = birthdayDate.getUTCDate();
 var year = birthdayDate.getUTCFullYear();
 var confirmDate = `${month} / ${day} / ${year}`;
-
+// Asks user to confirm their birthday
 var confirmBirthday = confirm(`Is this correct?\n${confirmDate}`);
