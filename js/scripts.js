f9687ce61d60dc6e5a37ee89e4297a1b1e662c02
BEN RECKAS <benreckas@BENs-MacBook-Pro.local>
2017-11-06 18:22:52 -0800

    In class review

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,9 @@ var firstName = prompt("What is your first name?");
 console.log(firstName);
 // Asks for users last name and alerts the answer in the window
 var lastName = prompt("What is your last name?");
-var alertName = alert(`Whassup, ${lastName}!`);
+alert(`Whassup, ${lastName}!`);
 // Asks for users birthday
-var birthday = prompt("Gimme your birthday.\nMM/DD/YYYY");
+var birthday = prompt("Gimme your birthday.", 'MM/DD/YYYY');
 // Inserts users birthday into Date object
 var birthdayDate = new Date(birthday);
 // Retrieves the month day and year and assigns them in a string to confirmDate
@@ -14,4 +14,4 @@ var day = birthdayDate.getUTCDate();
 var year = birthdayDate.getUTCFullYear();
 var confirmDate = `${month} / ${day} / ${year}`;
 // Asks user to confirm their birthday
-var confirmBirthday = confirm(`Is this correct?\n${confirmDate}`);
+var isBirthdayCorrect = confirm(`Is this correct?\n${confirmDate}`);
