bf56a93875b9d33260b19bc53f41cc277a9e3a72
benreckas <benreckas@gmail.com>
2017-11-15 13:59:34 -0800

    Finished the capitalize and the reverse functions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,36 @@
 //Square Root Function
 
+sqNum(prompt("Give me a number. I'll square it.", "9"));
 
-// sqrt(prompt("Give me a number. I'll return it's square root."));
-//
-// function sqrt(num) {
-//   num = parseInt(num);
-//   if (isNaN(num)) {
-//     alert(`Sorry, that isn't a number!`);
-//     sqrt(prompt("Give me a number. I'll return it's square root."));
-//   }
-//   num = Math.sqrt(num);
-//   console.log(num);
-// };
-
+function sqNum(num) {
+  num = parseInt(num);
+  if (isNaN(num)) {
+    alert(`Sorry, that isn't a number!`);
+    sqNum(prompt("Give me a number. I'll return it's square root."));
+  }
+  num = num * num;
+  return(alert(`Your number squared is equal to ${num}`));
+};
 
 // Capitalize Function
 
-
-var userStr = prompt("Tell me your secrets.");
-capitalize(userStr);
+capitalize(prompt("Tell me something, yo."));
 
 function capitalize(str) {
-  str = str.toLowerCase();
-  var arrStr = str.split('.');
-  var newStr;
-  for(let i = 0; i <= arrStr.length; i++) {
-  newStr += arrStr[i].charAt(0).toUpperCase() + arrStr[i].slice(1);
-  };
-  return console.log(newStr);
+  str = str.toLowerCase().split('. ');
+  for (var i = 0; i < str.length; i++) {
+    str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+    str[i] = `${str[i]}.`;
+  }
+  return alert(str.join(' '));
 };
 
-capitalize();
+// Flip the first and second half of a String Function.
+
+reverseStr(prompt("Tell me a word and I'll flip the front and back half.", "Yin Yang"));
+
+function reverseStr(str) {
+  var half = Math.floor(str.length / 2);
+  var newStr = str.substr(half) + str.substr(0, half);
+  return alert(newStr);
+};
