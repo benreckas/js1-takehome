481c04d84b10c035e55ae87e497e4fdb919879f9
benreckas <benreckas@gmail.com>
2017-11-27 13:57:50 -0800

    Completed calculator.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,23 @@
-// + - * /
-// basic nums 0-9 and =
-// num mem store and recall
+const view = document.querySelector('#calcView');
+var store;
+
+function calcView(btnInput) {
+  view.innerHTML += btnInput;
+  if (btnInput === 'c') {
+    view.innerHTML = '';
+  }
+};
+
+function calc() {
+  let answer = view.innerHTML;
+  answer = eval(answer);
+  view.innerHTML = answer;
+};
+
+function mem() {
+  store = view.innerHTML;
+};
+
+function recall() {
+  view.innerHTML = store;
+};
