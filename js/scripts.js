3c6165beb7b7b28d3daabb4dbe76a724188f025b
benreckas <benreckas@gmail.com>
2017-11-28 18:10:42 -0800

    Added comments and removed unnecessary variable.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
+// DOM Elements
 const btn = document.querySelector('#input');
 const colorPick = document.querySelector('#color');
 const main = document.querySelector('main');
-var color = colorPick;
 
+// Prompt the user to enter a header and a paragraph to append to the page
 function addContent() {
   btn.className = 'hidden';
   const h1Str = prompt('Add Header', 'Baby Driver');
@@ -10,18 +11,18 @@ function addContent() {
   const h1 = document.createElement('h1');
   const p = document.createElement('p');
   h1.textContent = h1Str;
-  h1.className = 'center'
+  h1.className = 'center';
   p.textContent = pStr;
   p.className = 'justify';
   main.append(h1, p);
 };
 
+// Ask the user for a color and change the background color of the paragraph to that color
 function addColor() {
   const p = document.querySelector('p');
   var color;
   colorPick.addEventListener('change', (e) => {
     color = e.target.value;
-    console.log(e, color);
     p.style.backgroundColor = color;
   });
 };
